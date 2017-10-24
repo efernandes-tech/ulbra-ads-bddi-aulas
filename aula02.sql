b1d93ab1bf68de949f741bb89c2d781c65b1ec2b
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-10-24 19:38:46 -0200

    Update aula02.sql

--- aula02.sql
+++ aula02.sql
@@ -1,11 +1,13 @@
-create table TbCliente (
-    PkCodCli integer not null,
-    NomeCli varchar(30) not null,
-    EndCli varchar(30),
-    DataNascCli date,
-    SalarioCli number(15,2),
-    SexoCli varchar(1)
+CREATE TABLE TbCliente (
+    PkCodCli INTEGER NOT NULL,
+    NomeCli VARCHAR(30) NOT NULL,
+    EndCli VARCHAR(30),
+    DataNascCli DATE,
+    SalarioCli NUMBER(15,2),
+    SexoCli VARCHAR(1)
 );
 
-alter table TbCliente add constraint TbClientePk primary key(PkCodCli);
+ALTER TABLE TbCliente ADD CONSTRAINT TbClientePk PRIMARY KEY(PkCodCli);
 
+INSERT INTO TbCliente(PkCodCli, NomeCli, EndCli, DataNascCli, SalarioCli, SexoCli)
+VALUES(1, 'Maria Antonieta', 'Rua Sei Lá, Nº 43', '20/08/2000', 1250.43, 'F');
