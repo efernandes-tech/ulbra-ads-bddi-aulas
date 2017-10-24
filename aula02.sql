49385f967f5bd0134b3d84a7c82349ed5f3d0cec
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-10-24 19:18:46 -0200

    Create aula02.sql

---
+++ aula02.sql
@@ -0,0 +1,9 @@
+create table TbCliente (
+    PkCodCli integer not null,
+    NomeCli varchar(30) not null,
+    EndCli varchar(30),
+    DataNascCli date,
+    SalarioCli number(15,2),
+    SexoCli varchar(1)
+);
+
