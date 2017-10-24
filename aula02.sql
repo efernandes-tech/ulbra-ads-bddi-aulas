465cbbba9d6e70cd51d5e6a76c86f591c12176d4
Éderson Fernandes <edersonluis.rf@gmail.com>
2017-10-24 20:09:29 -0200

    Update aula02.sql

--- aula02.sql
+++ aula02.sql
@@ -10,4 +10,8 @@ CREATE TABLE TbCliente (
 ALTER TABLE TbCliente ADD CONSTRAINT TbClientePk PRIMARY KEY(PkCodCli);
 
 INSERT INTO TbCliente(PkCodCli, NomeCli, EndCli, DataNascCli, SalarioCli, SexoCli)
-VALUES(1, 'Maria Antonieta', 'Rua Sei Lá, Nº 43', '20/08/2000', 1250.43, 'F');
+    VALUES(1, 'Maria Antonieta', 'Rua Sei Lá, Nº 43', '20/08/2000', 1250.43, 'F');
+
+SELECT * FROM TbCliente;
+
+SELECT PkCodCli, NomeCli FROM TbCliente WHERE PkCodCli;
